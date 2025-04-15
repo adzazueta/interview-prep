b8772aa022834e40234f6f363b4596f5b68e685b
Alexis D. Zazueta <alexisd.zazueta@gmail.com>
2025-04-14 23:23:11 -0600

    feat(agent): add functionality to agent component

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,11 +1,16 @@
 // Components
 import Agent from '@/components/Agent';
 
-const Page = () => {
+// Actions
+import { getCurrentUser } from '@/lib/actions/auth.actions';
+
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h3>Interview Generation</h3>
-      <Agent userName="You" userId="user1" type="generate" />
+      <Agent userName={user?.name ?? ''} userId={user?.id} type="generate" />
     </>
   );
 }
